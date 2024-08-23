04d777073e696b410ed4b4ba3fa52143beb82716
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-22 17:15:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ def get_youtube_videos(api_key, channel_id):
     request = youtube.search().list(
         part="snippet",
         channelId=channel_id,
-        maxResults=10000,  # Adjust the number of videos retrieved
+        # maxResults=10000,  # Adjust the number of videos retrieved
         order="date"  # Sort by publication date
     )
     response = request.execute()
@@ -31,7 +31,7 @@ st.title("YouTube Channel Video List")
 st.write("Enter your YouTube API Key and Channel ID to get the video list.")
 
 api_key = st.text_input("YouTube API Key", type="password")
-channel_id = st.text_input("YouTube Channel ID", value="UCAuUUnT6oDeKwE6v1NGQxug")
+channel_id = st.text_input("YouTube Channel ID", value="UCLRAP5fUb-OpHEiTryypa0g")
 
 if st.button("Get Videos"):
     if api_key and channel_id:
