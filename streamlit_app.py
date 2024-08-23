7e120c7bc6d6cd05645390c726cc37d5e965ef7d
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-23 16:53:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,38 @@ def get_youtube_videos(api_key, channel_id):
     
     return pd.DataFrame(videos)
 
-# Streamlit UI
-st.title("YouTube Channel Video List")
+
+
+# Setting page layout
+st.set_page_config(
+    page_title="YouTube Channel Video Title List",
+    page_icon="✨",
+    layout="centered",
+    initial_sidebar_state="expanded"
+)
+
+# Sidebar for API Key and User Info
+st.sidebar.header("About App")
+st.sidebar.markdown('This is an app that queries the YouTube API and returns a list of all videos for particular YouTube channel created by <a href="https://ai.jdavis.xyz" target="_blank">0xjdavis</a>.', unsafe_allow_html=True)
+
+# Calendly
+st.sidebar.markdown("""
+    <hr />
+    <center>
+    <div style="border-radius:8px;padding:8px;background:#fff";width:100%;">
+    <img src="https://avatars.githubusercontent.com/u/98430977" alt="Oxjdavis" height="100" width="100" border="0" style="border-radius:50%"/>
+    <br />
+    <span style="height:12px;width:12px;background-color:#77e0b5;border-radius:50%;display:inline-block;"></span> <b>I'm available for new projects!</b><br />
+    <a href="https://calendly.com/0xjdavis" target="_blank"><button style="background:#126ff3;color:#fff;border: 1px #126ff3 solid;border-radius:8px;padding:8px 16px;margin:10px 0">Schedule a call</button></a><br />
+    </div>
+    </center>
+    <br />
+""", unsafe_allow_html=True)
+
+# Copyright
+st.sidebar.caption("©️ Copyright 2024 J. Davis")
+
+st.title("YouTube Channel Video Title List")
 
 api_key = st.secrets["youtube_key"] #st.text_input("YouTube API Key", type="password")
 channel_id = st.text_input("YouTube Channel ID", value="UCLRAP5fUb-OpHEiTryypa0g")
